e674a93a8f011c2d78d50bd33fe89c14f967c8d1
Ritesh <ritessshhh7@gmail.com>
2025-01-22 12:08:07 +0530

    add email field model

--- Server/models/User.js
+++ Server/models/User.js
@@ -3,6 +3,11 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 3, maxlength: 50 },
   password: { type: String, required: true, minlength: 6 },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   mobile: {
     type: String,
     required: true,
