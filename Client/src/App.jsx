fdffc7fd54cc49e549b07976976cd7b2cec940bd
maheshraut07 <rautmahesh213@gmail.com>
2025-01-22 13:33:46 +0530

    login and signup page added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,3 +1,5 @@
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
 import Home from './components/Home'
 import { Button } from './components/ui/button'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
@@ -7,7 +9,16 @@ const appRouter = createBrowserRouter([
   {
     path : '/',
     element: <Home/>
-  }
+  },
+  {
+    path : '/login',
+    element: <Login/>
+  },
+  {
+    path : '/signup',
+    element: <Signup/>
+  },
+  
 ])
 
 function App() {
