82c11065333ec57ed14c47efd750f677da02c614
Ritesh <ritessshhh7@gmail.com>
2025-01-22 12:07:48 +0530

    add email validation

--- Server/middlewares/validation.js
+++ Server/middlewares/validation.js
@@ -3,6 +3,7 @@ import { body } from "express-validator";
 export const registerValidators = [
   body("name", "Name cannot be empty").notEmpty(),
   body("mobile", "Phone number cannot be empty").notEmpty(),
+  body("email", "Invalid email").isEmail(),
   body("mobile", "Invalid phone number").isMobilePhone("en-IN"),
   body("password", "Password must be of minimum 4 characters").isLength({
     min: 4,
