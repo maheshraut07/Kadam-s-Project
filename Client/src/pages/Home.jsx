aab4355f5c620a97c9bb41b9208f420aef4577a0
Ritesh <ritessshhh7@gmail.com>
2025-01-24 11:29:22 +0530

    implement fetch items

---
+++ Client/src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
+import HeroSection from "../components/HeroSection";
+import Grapes from "../components/Grapes";
+import itemsApi from "@/api/modules/api.item";
+import { toast } from "react-toastify";
+import Loader from "@/components/ui/Loader";
+
+const Home = () => {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    const getAllItems = async () => {
+      const { response, error } = await itemsApi.getItems();
+      if (response) {
+        setItems(response.items);
+        console.log(response);
+      }
+      if (error) {
+        toast.error(error.message);
+      }
+    };
+
+    getAllItems();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <Grapes items={items} />
+      {/* <Footer/> */}
+      <Loader loading={!items} />
+    </div>
+  );
+};
+
+export default Home;
